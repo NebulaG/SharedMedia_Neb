2a34242a04aca009707c8fbbe69b485edea86f13
NebulaG <58909194+NebulaG@users.noreply.github.com>
2026-05-10 15:56:54 +0100

    Add files via upload

---
+++ RegisterSounds.lua
@@ -0,0 +1,25 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+local mediaPath = "Interface\\AddOns\\SharedMedia_Neb\\sounds\\"
+
+
+LSM:Register("sound", "Neb - Aura Pulse", mediaPath .. "aura_pulse.ogg")
+LSM:Register("sound", "Neb - Bomboclat", mediaPath .. "bomboclat.ogg")
+LSM:Register("sound", "Neb - Bosh", mediaPath .. "bosh.ogg")
+LSM:Register("sound", "Neb - Cueball", mediaPath .. "cueball.ogg")
+LSM:Register("sound", "Neb - Delete The Gaze", mediaPath .. "delete_the_gaze.ogg")
+LSM:Register("sound", "Neb - Donald Trump Im Gonna Come", mediaPath .. "donald_trump_im_gonna_come.ogg")
+LSM:Register("sound", "Neb - Fart Big", mediaPath .. "fart big.ogg")
+LSM:Register("sound", "Neb - Fart Reverb", mediaPath .. "fart reverb.ogg")
+LSM:Register("sound", "Neb - Fart Bigger", mediaPath .. "fart_bigger.ogg")
+LSM:Register("sound", "Neb - Fart Small", mediaPath .. "fart_small.ogg")
+LSM:Register("sound", "Neb - Gay", mediaPath .. "gay.ogg")
+LSM:Register("sound", "Neb - Heroism", mediaPath .. "heroism.ogg")
+LSM:Register("sound", "Neb - Hes Done It", mediaPath .. "hes_done_it.ogg")
+LSM:Register("sound", "Neb - Huh", mediaPath .. "huh.ogg")
+LSM:Register("sound", "Neb - Metal Pipe", mediaPath .. "metal_pipe.ogg")
+LSM:Register("sound", "Neb - Pufferfish", mediaPath .. "pufferfish.ogg")
+LSM:Register("sound", "Neb - Two Hours Later", mediaPath .. "two_hours_later.ogg")
+LSM:Register("sound", "Neb - Vine Boom", mediaPath .. "vine_boom.ogg")
+LSM:Register("sound", "Neb - Waheey", mediaPath .. "waheey.ogg")
+LSM:Register("sound", "Neb - Why Are You Gay", mediaPath .. "why_are_you_gay.ogg")
